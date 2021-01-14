ffe98c4b5d802583a4ce9c4cba37532045c1e4dc
burugaria7 <burugaria@icloud.com>
2021-01-14 21:03:52 +0900

    追跡解除

--- webgame/minesweeper/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'minesweeper'
-urlpatterns = [
-    # ex: minesweeper/
-    path('', views.index, name='index'),
-]
--- webgame/tictactoe/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'tictactoe'
-urlpatterns = [
-    # ex: tictactoe/
-    path('', views.index, name='index'),
-]
